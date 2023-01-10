3fdad98da8586ac5962302312553a46a38a3b94b
jeelion22 <jeelion22@gmail.com>
2023-01-10 18:12:49 +0530

    change day_calulator.py from test.py

---
+++ day_calculate.py
@@ -0,0 +1,23 @@
+import datetime
+
+start_date = input()
+end_date = input()
+
+saturdays = 0
+sundays = 0
+duration = datetime.timedelta(days=1)
+
+start_date_object = datetime.datetime.strptime(start_date, "%d %b %Y")
+end_date_object = datetime.datetime.strptime(end_date, "%d %b %Y")
+
+date = start_date_object - duration
+while date < end_date_object:
+    date = date + duration
+    print(date)
+    day = datetime.datetime.strftime(date, "%A")
+    if day == "Saturday":
+        saturdays += 1
+    if day == "Sunday":
+        sundays += 1
+print("Saturdays: {}".format(saturdays))
+print("Sunday: {}".format(sundays))
